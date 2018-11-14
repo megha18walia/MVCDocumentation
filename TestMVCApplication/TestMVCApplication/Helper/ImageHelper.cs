9574f8dd951999e832d6d8efc44bf387f35ec991
Megha Walia <mwalia2@sapient.com>
2018-11-14 09:00:51 +0000

    code updated for Helpers

---
+++ TestMVCApplication/TestMVCApplication/Helper/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TestMVCApplication.Helper
+{
+    public static class ImageHelper
+    {
+        public static string Image(this HtmlHelper helper, string id, string url, string altText, int height, int width)
+        {
+            return Image(helper, id, url, altText, null,height, width);
+        }
+
+        public static string Image(this HtmlHelper helper, string id, string url, string altText, object htmlAttr, int height, int width)
+        {
+            var builder = new TagBuilder("img");
+            builder.GenerateId(id);
+            builder.MergeAttribute("src", url);
+            builder.MergeAttribute("alt", altText);
+            builder.MergeAttribute("height", height.ToString());
+            builder.MergeAttribute("width", width.ToString());
+            builder.MergeAttributes(new RouteValueDictionary(htmlAttr));
+            string s = builder.ToString(TagRenderMode.SelfClosing);
+            return s;
+        }
+    }
+}
---
+++ TestMVCApplication/TestMVCApplication/Helper/LabelHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVCApplication.Helper
+{
+    public static class LabelHelper
+    {
+        public static string Label(this HtmlHelper helper, string target, string text, int num)
+        {
+            string s = $"<label for = {target}>{text}</label>";
+            return s;
+        }
+    }
+}
